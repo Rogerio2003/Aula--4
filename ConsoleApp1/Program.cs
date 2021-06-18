96cc0ecc2b8d6937761a58d577f01e825365b226
Rogerio Melo <rogerio@inovebr.com>
2021-06-18 16:04:10 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Acesso();
+
+            //Console.ReadKey();
+
+
+            Program p = new Program();
+
+            double salario ;
+
+            Console.Write("Informe seu salario atual: ");
+
+            double.TryParse(Console.ReadLine(),out salario);
+
+            Console.WriteLine();
+
+            Console.Write("salario: ");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write(salario.ToString());
+
+            Console.ResetColor();
+
+                Console.WriteLine();
+
+            Console.Write(" Novo salario: ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            
+
+            
+
+            Console.Write(p.Novosalario(salario));
+
+            Console.ReadKey();
+
+
+        }
+
+        private double Novosalario(double salario)
+        {
+            double resultado = 0;
+
+            if (salario <= 1700)
+            {
+                resultado = 300 + salario;
+            }
+            else
+            {
+                resultado = 200 + salario;
+
+            }
+            return resultado;
+        }
+
+        //private static void Acesso()
+        //{
+        //    int idade;
+
+        //    Console.Write("informe sua idade para ter acesso :");
+
+        //    int.TryParse(Console.ReadLine(), out idade);
+
+        //    if (idade >= 18)
+        //    {
+        //        Console.ForegroundColor = ConsoleColor.Green;
+        //        Console.WriteLine("acesso liberado ");
+        //    }
+        //    else
+        //    {
+        //        Console.ForegroundColor = ConsoleColor.Red;
+        //        Console.WriteLine("acesso negado ");
+        //    }
+        //}
+
+
+
+    }
+}
